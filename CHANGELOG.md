797623cdd3665acef87b0a60f4209e9ac8ff2544
Eric Zieger <github@eric-zieger.de>
2015-10-08 21:21:24 +0200

    adds grunt task configs and changes folder structure

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 #CHANGELOG
 
+ON THE ROAD TO MORE SATISFYING v1.0.0
+
+* **feature/grunt-and-miscellaneous:** Adds a simple grunt workflow, and misc dev files
 * **initial commit:** Initial upload of Planoker WebApp mockup.
--- Gruntfile.js
+++ Gruntfile.js
@@ -18,15 +18,68 @@ module.exports = function (grunt) {
   grunt.initConfig({
 
     // Project settings
+    pkg: grunt.file.readJSON('package.json'),
     config: config,
+
+    // JS related configs
+
+    concat: {
+      dist: {
+        src: [
+          'node_modules/jquery/dist/jquery.js',
+          '<%= config.app %>/js/**/*.js'
+        ],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= config.dist %>/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    // CSS related configs
+
+    cssmin: {
+      target: {
+        files: {
+          '<%= config.dist %>/<%= pkg.name %>.min.css':
+            [
+              'node_modules/normalize.css/normalize.css',
+              '<%= config.app %>/**/*.css'
+            ]
+        }
+      }
+    },
+
+    // APPCACHE related configs
+    manifest: {
+      generate: {
+        options: {
+          basePath: './',
+          headcomment: " <%= pkg.name %> v<%= pkg.version %>",
+          hash: true,
+          master: ['index.html']
+        },
+        src: [
+          'index.html',
+          '<%= config.dist %>/media/*.*',
+          '<%= config.dist %>/*.min.js',
+          '<%= config.dist %>/*.min.css'
+        ],
+        dest: 'manifest.appcache'
+      }
+    }
 
   });
 
-  grunt.registerTask('build', [
-    '',
-  ]);
+  grunt.registerTask('build', ['concat', 'uglify', 'cssmin', 'manifest']);
 
-  grunt.registerTask('default', [
-    'build'
-  ]);
+  grunt.registerTask('default', ['build']);
 };
